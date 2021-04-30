896b221f3409ba03b8d45e5152b337c5f571434a
jeff-mcc <reffery@gmail.com>
2021-04-29 23:10:45 -0600

    completed base code for server

---
+++ small-business-platform/server/controllers/inventoryController.js
@@ -0,0 +1,61 @@
+let inventory = [
+    {
+        id: 1,
+        name: "Fuzzy Bear's toilet paper: 4 pack",
+        price: 4.99,
+        quantity: 50,
+        available: true,
+        sale: false,
+        salePrice: null,
+        description: "So soft it feels like you are wiping your rear end with the fuzzy backside of a baby bear. Just keep an eye out for those 'mamma bear' customers who want it for themselves!",
+        imageURL: "https://media.istockphoto.com/photos/white-roll-toilet-paper-on-the-light-blue-background-picture-id901805432?k=6&m=901805432&s=612x612&w=0&h=pX3erJkw7H539co6KXpEbae1SdsZ-wn1Z1EEyREcfjE=",
+        categories: {
+            cat1: 'toiletries',
+            cat2: 'household items'
+        }
+    }
+];
+
+let id = 2;
+
+module.exports = {
+    getItems: (req,res)=>{
+        res.status(200).send(inventory)
+    },
+    addItem: (req,res)=>{
+        const {name,price,quantity,description,imageURL,categories} = req.body;
+        const avail = quantity>0 ? true : false;
+        const newItem = {
+            id,
+            name,
+            price,
+            quantity,
+            available: avail,
+            sale: false,
+            salePrice: null,
+            description,
+            imageURL,
+            categories
+        }
+        id++
+        inventory.push(newItem)
+        res.status(200).send(inventory)
+    },
+    deleteItem: (req,res)=>{
+        const idx = +req.params.id;
+        inventory = inventory.filter(e=>e.id!==idx)
+        res.status(200).send(inventory)
+    },
+    editItem: (req,res)=>{
+        let {quantity,salePrice} = req.body;
+        let idx = +req.params.id;
+        const avail = quantity>0 ? true : false;
+        salePrice = salePrice>0 ? salePrice : null;
+        const index = inventory.findIndex(e=>e.id===idx);
+        inventory[index].available = avail;
+        inventory[index].quantity = quantity;
+        inventory[index].salePrice = salePrice;
+        inventory[index].sale = salePrice===null ? false : true;
+        res.status(200).send(movies)
+    }
+}
---
+++ small-business-platform/server/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express()
+const port = 3030;
+
+const inventoryCtrl = require('./controllers/inventoryController')
+
+app.use(express.json())
+
+app.get('/smallbusiness/store', inventoryCtrl.getItems)
+app.post('/smallbusiness/store', inventoryCtrl.addItem)
+app.delete('/smallbusiness/store/:id', inventoryCtrl.deleteItem)
+app.put('/smallbusiness/store/:id', inventoryCtrl.editItem)
+
+app.listen(port,()=>console.log(`Server is eating tacos on port: ${port}`))
